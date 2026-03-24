e4587abfe8d93afd3b6e1bb895a39636a5eb1f40
manideep1428 <saimanideep.ch12345@gmail.com>
2026-03-24 10:59:17 +0530

    fixed guide files

--- schemas_list.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-openapi_path = 'c:/Users/saima/OneDrive/Desktop/fiverr/docs/api-reference/openapi.json'
-with open(openapi_path, 'r', encoding='utf-8') as f:
-    d = json.load(f)
-
-schemas = list(d.get('components', {}).get('schemas', {}).keys())
-print(", ".join(schemas))
